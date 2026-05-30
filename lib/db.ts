3d76d7ce424313d7d1f8014ac096cbe09316850b
Ignacio Tomas <hermidatomy@gmail.com>
2026-05-30 10:37:56 -0300

    solucionando conexion con la base de datos para deploy

--- lib/db.ts
+++ lib/db.ts
@@ -5,7 +5,7 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 function createPrismaClient() {
   const adapter = new PrismaPg({ 
-    connectionString: process.env.DIRECT_URL!
+    connectionString: process.env.DATABASE_URL!
   });
   return new PrismaClient({ adapter });
 }
